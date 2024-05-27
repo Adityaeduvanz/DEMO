7399d234a8b6693ac6c1891dc83baae24fc4e934
Adityaeduvanz <aditya.jangid@eduvanz.com>
2024-05-27 16:34:39 +0530

    Update helloo11.java

--- helloo11.java
+++ helloo11.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello241, world!2e");
+        System.out.println("Hello2641, world!2e");
     }
 }
